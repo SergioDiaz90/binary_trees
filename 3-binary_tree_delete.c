c2eb786578872f5c67ea95ffd08dd73752b519df
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-20 10:29:12 -0500

    [Fix]: without conditinals for use recurtions

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,12 +10,9 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (!tree)
 		return;
 
-	if (tree->left == NULL && tree->right == NULL)
-		del_node(tree);
-	else if (tree->left != NULL)
-		binary_tree_delete(tree->left);
-	else if (tree->right != NULL)
-		binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	del_node(tree);
 }
 
 void del_node(binary_tree_t *node)
