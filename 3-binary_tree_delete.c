a9f23a0f29dfb3a0b857418ef567a8659d452b06
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-20 10:33:08 -0500

    [Add]: New documentation for functions

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: the pointer to the parent node of the node to create
- * @value: the value of the this new node
- * Return: a pointer to the new node or NULL on failure
+ * binary_tree_delete - del tree binnary.
+ * @tree: pointer the tree for delete
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
@@ -14,6 +13,11 @@ void binary_tree_delete(binary_tree_t *tree)
 	binary_tree_delete(tree->right);
 	del_node(tree);
 }
+
+/**
+ * del_node - Destroy nodes.
+ * @node: pointer to node for destroy.
+ */
 
 void del_node(binary_tree_t *node)
 {
