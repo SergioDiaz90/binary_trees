451407a3358fd310d91b1d7423f81cbebe240b17
juandsuarezz <1486@holbertonschool.com>
2020-07-21 23:49:19 +0000

    [Add]: Task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * levelfromroot - iterates backward until the root node is found
+ * @node: the current node being analyzed
+ * @level: the level counter
+ */
+size_t levelfromroot(const binary_tree_t *node, size_t level)
+{
+	if (node->parent == NULL)
+		return (level);
+
+	else
+		return (level + levelfromroot(node->parent, 1));
+}
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @node: the node to measure the depth of
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t count;
+
+	if (node == NULL || node->parent == NULL)
+		return (0);
+
+	count = levelfromroot(node->parent, 1);
+	return (count);
+}
