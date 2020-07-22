3432092243e41e71b5425fe9f5c7f5f646cd5d1f
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-22 16:21:18 -0500

    [Add]: Posiblecorrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,14 +8,14 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (!parent)
+	{
+		free(new_node);
 		return (NULL);
-
-	new_node = binary_tree_node(parent, value);
-
-	if (parent->left == NULL)
+	}
+	else if (parent->left == NULL)
 	{
 		new_node->parent = parent;
 		parent->left = new_node;
