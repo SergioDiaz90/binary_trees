70eef4f5dbef3d40fe2877c5776fff783a92567e
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-22 17:23:59 -0500

    [Fix]: Recursion

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,8 +11,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if ((!tree->left) && (!tree->right))
+		return (1);
+
 	if ((tree->left != NULL) && (tree->right != NULL))
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	else
-		return (1);
+		return (0);
 }
