b2be01f5301e8bb229ba81c43703c02d9f7ce793
juandsuarezz <1486@holbertonschool.com>
2020-07-22 20:02:36 +0000

    [Fix]: Betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,6 +2,7 @@
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: a pointer to the tree to analyze
+ * Return: the number of leaves or 0 if tree is NULL
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
