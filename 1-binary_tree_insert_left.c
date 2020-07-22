31662bbb052c93731cdb365fcb7c7c6194f82f3e
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-22 16:33:34 -0500

    [Fix]: New corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,21 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_node = NULL;
 
-	if (!parent)
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (!parent)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	if (parent->left == NULL)
 	{
