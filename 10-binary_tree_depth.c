10089a22ad135bb47e156edc0e0071e81471f477
juandsuarezz <1486@holbertonschool.com>
2020-07-22 20:00:12 +0000

    [Fix]: Betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,6 +3,7 @@
  * levelfromroot - iterates backward until the root node is found
  * @node: the current node being analyzed
  * @level: the level counter
+ * Return: the level from the root node to the current node
  */
 size_t levelfromroot(const binary_tree_t *node, size_t level)
 {
@@ -16,6 +17,7 @@ size_t levelfromroot(const binary_tree_t *node, size_t level)
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @node: the node to measure the depth of
+ * Return: the depth of the node or 0 if node is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
